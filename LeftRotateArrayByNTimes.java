c009fa102ab7ff0b2960cb3fc14e5e0339519d6c
PraveenSS3B <praveenkumar66118@gmail.com>
2023-10-02 22:07:38 +0530

    Solved using Reversal Algorithm > T.C - O(n); A.S - O(1)

--- LeftRotateArrayByNTimes.java
+++ LeftRotateArrayByNTimes.java
@@ -7,11 +7,18 @@ public class LeftRotateArrayByNTimes {
 	public static void main(String[] args) {
 		int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
 
+		System.out.println("Original Array :\n" + Arrays.toString(arr));
+
 		int d = 3;
 
 		arr = rotateLeftByD(arr, d, arr.length);
 
-		System.out.println(Arrays.toString(arr));
+		System.out.println("\nRotated Array using Best Method :\n" + Arrays.toString(arr));
+
+		int arr1[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+		rotate(arr1, d, arr.length);
+		System.out.println("\nRotated Array using Efficient Method :\n" + Arrays.toString(arr1));
 	}
 
 	private static int[] rotateLeftByD(int[] arr, int d, int n) {
@@ -33,4 +40,22 @@ public class LeftRotateArrayByNTimes {
 		return arr;
 	}
 
+	private static void rotate(int[] arr, int d, int n) {
+
+		d = d % n;
+		reverse(arr, 0, d - 1);
+		reverse(arr, d, n - 1);
+		reverse(arr, 0, n - 1);
+	}
+
+	private static void reverse(int[] arr, int low, int high) {
+		while (low < high) {
+			int tmp = arr[low];
+			arr[low] = arr[high];
+			arr[high] = tmp;
+
+			low++;
+			high--;
+		}
+	}
 }
