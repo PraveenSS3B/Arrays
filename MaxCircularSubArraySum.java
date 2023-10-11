3849fa58411f6148db6373cfc772ebf38cb10be0
PraveenSS3B <praveenkumar66118@gmail.com>
2023-10-11 23:09:08 +0530

    T.C - O(n); A.S - O(1)

--- MaxCircularSubArraySum.java
+++ MaxCircularSubArraySum.java
@@ -3,9 +3,49 @@ package in.Arrays;
 public class MaxCircularSubArraySum {
 
 	public static void main(String[] args) {
-		int arr[] = { 10, 5, -5 };
+//		int arr[] = { 10, 5, -5 };
 
-		System.out.println(find(arr, arr.length));
+//		int arr[] = { 8, -4, 3, -5, 4 };
+
+		int arr[] = { -3, -18, -22, -21, -17, 16, -14, 28, -22 };
+
+		System.out.println("Naive Approach : " + find(arr, arr.length));
+
+		System.out.println("Efficient Approach : " + getMaxCirSum(arr, arr.length));
+
+	}
+
+	private static int normalMaxSubArraySum(int[] arr, int n) {
+		int res = arr[0];
+
+		int cs = arr[0];
+
+		for (int i = 1; i < n; i++) {
+			cs = Math.max(arr[i], arr[i] + cs);
+
+			res = Math.max(cs, res);
+		}
+
+		return res;
+	}
+
+	private static int getMaxCirSum(int[] arr, int n) {
+
+		int ns = normalMaxSubArraySum(arr, n);
+
+		if (ns < 0)
+			return ns;
+
+		int arrSum = 0;
+
+		for (int i = 0; i < n; i++) {
+			arrSum += arr[i];
+			arr[i] = -arr[i];
+		}
+
+		int circularSum = arrSum + normalMaxSubArraySum(arr, n);
+
+		return Math.max(ns, circularSum);
 
 	}
 
