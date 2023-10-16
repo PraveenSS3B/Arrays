f4cb56ef9f19ce86bae4ed8eaa70e3742b0bcb42
PraveenSS3B <praveenkumar66118@gmail.com>
2023-10-16 23:52:41 +0530

    Efficient Approach -> T.C - O(n + max) where max is the freq array size; A.S - O(max)

--- MaxAppearingElement.java
+++ MaxAppearingElement.java
@@ -7,6 +7,31 @@ public class MaxAppearingElement {
 		int right[] = { 5, 8, 7, 18 };
 
 		System.out.println(find(left, right, left.length));
+
+		System.out.println(findEfficient(left, right, left.length));
+	}
+
+	// Efficient Approach - T.C - O(n + MAX), where Max is Freq array size
+	private static int findEfficient(int[] left, int[] right, int n) {
+
+		int freq[] = new int[101];
+
+		for (int i = 0; i < n; i++) {
+			freq[left[i]]++;
+			freq[right[i] + 1]--;
+		}
+		
+		int res = 0;
+		
+		for(int i = 1; i < 100; i++)
+		{
+			freq[i] = freq[i - 1] + freq[i];
+			
+			if(freq[i]  >freq[res])
+				res = i;
+		}
+
+		return res;
 	}
 
 	// Naive - T.C - O(n * MAX), where Max is Freq array size
